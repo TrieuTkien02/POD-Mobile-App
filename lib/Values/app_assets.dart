7f21dacda81958d681e81b99665cdb930e578a03
Duongquochai20521276 <80502386+Duongquochai20521276@users.noreply.github.com>
2023-05-20 23:48:45 +0700

    UI-Trang chủ

---
+++ lib/Values/app_assets.dart
@@ -0,0 +1,7 @@
+
+class AppAssets{
+  static final String imagePath = 'assets/images/';
+  static final String anhbia = imagePath + 'AnhBia.jpg';
+  static final String anhdaidien = imagePath + 'AnhDaiDien.jpg';
+  static final String anhsanpham = imagePath + 'AnhSanPham.jpg';
+}
---
+++ lib/Values/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class AppColors{
+  static const Color backgroundColor = Color(0xffCCCCCC);
+  static const Color black = Color(0xff000000);
+  static const Color red = Color(0xffFF0000);
+  static const Color blue = Color(0xff778ACB);
+  static const Color white = Color(0xffFFFFFF);
+}
